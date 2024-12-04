45806f7d24c2214f40e64e12142463cb3d3c17ba
Shafee Ahmed <shafeelinks@gmail.com>
2024-12-03 19:01:21 -0500

    add alb dns name to outputs

--- terraform/ALB/outputs.tf
+++ terraform/ALB/outputs.tf
@@ -1,3 +1,8 @@
-output "alb_sg_id"{
+output "alb_sg_id" {
     value = aws_security_group.alb_sg.id
+}
+
+output "alb_dns_name" {
+    value = aws_lb.app_alb.dns_name
+    description = "DNS name of the application load balancer"
 }
